e0bb1e162a1e8324cf0f596439f7c1d45770aea5
Simpplay <Simpplay32@gmail.com>
2026-06-29 11:51:49 -0500

    Taller resuelto

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -5,6 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import pandas as pd
 
 def pregunta_12():
     """
@@ -22,3 +23,14 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
+    df = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+    
+    df["c5"] = df["c5a"].astype(str) + ":" + df["c5b"].astype(str)
+    
+    resultado = (
+        df.groupby("c0")["c5"]
+        .apply(lambda x: ",".join(sorted(x)))
+        .reset_index()
+    )
+    
+    return resultado
--- homework/pregunta_13.py
+++ homework/pregunta_13.py
@@ -5,6 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
+import pandas as pd
 
 def pregunta_13():
     """
@@ -20,3 +21,9 @@ def pregunta_13():
     E    275
     Name: c5b, dtype: int64
     """
+    tbl0 = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    tbl2 = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+    
+    merged_df = pd.merge(tbl0, tbl2, on="c0")
+    
+    return merged_df.groupby("c1")["c5b"].sum()
